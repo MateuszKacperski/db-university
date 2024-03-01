d811b082e635f589d59a7056c09a2ec7c7198bc6
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-03-01 13:34:52 +0100

    8 point

--- query_select.sql
+++ query_select.sql
@@ -28,4 +28,4 @@ SELECT COUNT(*) AS `tot_dipartimwenti` FROM `departments`;
 
 --8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
-
+SELECT * FROM `teachers` WHERE `phone` IS NULL;
