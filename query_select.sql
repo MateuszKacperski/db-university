fea031a4e664c76d1c81e9d26ddc8e1235612ed7
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-03-01 13:29:51 +0100

    5 point

--- query_select.sql
+++ query_select.sql
@@ -16,6 +16,7 @@ SELECT * FROM `courses` WHERE `year` = 1 And `period` = 'I semestre';
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
+SELECT * FROM `exams` WHERE HOUR(`hour`) >= 14 AND `date` = "2020-06-20";
 
 --6. Selezionare tutti i corsi di laurea magistrale (38)
 
